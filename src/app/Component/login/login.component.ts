96d7e89d02da56d0baed49920b0f933dca0c66d7
Darshan Joshi <36451926+ddjoshi@users.noreply.github.com>
2018-03-14 17:37:02 +0530

    11
    Some changes in both login and signup component

--- src/app/Component/login/login.component.ts
+++ src/app/Component/login/login.component.ts
@@ -1,54 +1,9 @@
-/*import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../../_services/index';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
-
-@Component({
-    moduleId: module.id,
-    templateUrl: 'login.component.html'
-})
-
-export class LoginComponent implements OnInit {
-    headers: Headers;
-    options: RequestOptions;
-    model: any = {};
-    loading = false;
-    error = '';
-
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService) { 
-          
-        }
-
-    ngOnInit() {
-        // reset login status
-        //this.authenticationService.logout();
-    }
-
-    login() {
-        this.loading = true;
-        this.authenticationService.login('peter@klaven','cityslicka')
-            .subscribe(result => {
-                if (result === true) {
-                    this.router.navigate(['']);
-                } else {
-                    this.error = 'Username or password is incorrect';
-                    this.loading = false;
-                }
-            });
-    }
-}*/
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../_models/User';
 import { Form,FormControl,FormGroup,FormBuilder,FormArray } from "@angular/forms";
-//import { UserServiceService } from "../../_services/user-service.service";
-
 import { AppRoutingModule } from "../../app-routing.module";
 import { Router } from '@angular/router';
-//import { Route } from '@angular/compiler/src/core';
 import { AuthenticationService } from '../../_services/index';
-//import { error } from 'util';
 
 @Component({
     moduleId: module.id,
@@ -65,11 +20,11 @@ export class LoginComponent implements OnInit {
   error:'';
 
   constructor(
-      private fb:FormBuilder,
+      private formBuilder:FormBuilder,
       private us1:AuthenticationService,
       private router:Router) { 
     
-    this.loginForm = this.fb.group({
+    this.loginForm = this.formBuilder.group({
 
       email:'',
       password:''
@@ -80,8 +35,6 @@ export class LoginComponent implements OnInit {
 
   login(){
 
-  
-        //this.us1.login('peter@klaven','cityslicka')
         this.us1.login('peter@klaven','cityslicka')
             .subscribe(result => {
                 if (result === true) {
@@ -93,21 +46,5 @@ export class LoginComponent implements OnInit {
             });
     
   }
-
-  /*prepareSaveUser1():currentUser
-  {
-    const formModel = this.loginForm.value;
-
-    const saveUser:User = {
-
-      //fname:this.siForm.get('fname').value as string,
-      //lname:this.signUpForm.get('lname').value as string,
-      email:this.loginForm.get('email').value as string,
-      password:this.loginForm.get('password').value as string
-
-    }
-    return saveUser;
-  }*/
-  
 
 }
